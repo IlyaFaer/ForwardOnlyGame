200ce2b1a692dff113c77e21a0650385467ec3b6
IlyaFaer <ilya.faer@mail.ru>
2020-02-18 21:06:50 +0300

    Model simple Train and calibrate moving along the paths. Closes #3.

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ class ForwardOnly(ShowBase):
         self._rail_blocks = self._load_rail_blocks()
 
         # load dummy model
-        box = self.loader.loadModel("models/bam/box.bam")
-        box.reparentTo(self.render)
+        train = self.loader.loadModel("models/bam/locomotive.bam")
+        train.reparentTo(self.render)
 
         # set Train node
         self._train = self.render.attachNewNode("Train")
-        box.wrtReparentTo(self._train)
+        train.wrtReparentTo(self._train)
 
         # start moving
-        self._move_along_next(box, is_start=True)
+        self._move_along_next(train, is_start=True)
 
-        self.cam.setPos(0, 0, 150)
-        self.cam.lookAt(box)
+        self.cam.setPos(8, 12, 15)
+        self.cam.lookAt(train)
 
     def _move_along_next(self, model, is_start=False):
         """Move model along the next motion path.
@@ -48,7 +48,7 @@ class ForwardOnly(ShowBase):
         model.wrtReparentTo(self._train)
 
         path_int2 = MopathInterval(
-            self._rail_blocks[name], model, duration=2, name="current_path"
+            self._rail_blocks[name], model, duration=10, name="current_path"
         )
         seq = Sequence(path_int2, Func(self._move_along_next, model))
         seq.start()
