b37744b5c0fa67461ef38f4f141b19860dbc52a9
IlyaFaer <ilya.faer@mail.ru>
2020-05-05 21:21:04 +0300

    Add camera rotation controls. Closes #15

--- camera_controller.py
+++ camera_controller.py
@@ -1,5 +1,5 @@
 """Camera configuring and controls."""
-from direct.interval.LerpInterval import LerpPosInterval
+from direct.interval.LerpInterval import LerpPosInterval, LerpHprInterval
 from panda3d.core import Vec3
 
 
@@ -9,6 +9,7 @@ class CameraController:
     def __init__(self):
         self._target = Vec3(2, 0, 3)  # final pos of the current movement
         self._move_int = None  # current move interval
+        self._turn_int = None  # current rotation interval
 
     def set_camera_controls(self, game, cam, train_node, train_mod):
         """Configure camera, its node and set keyboard keys to control the camera.
@@ -40,6 +41,12 @@ class CameraController:
         game.accept("arrow_down-up", self._stop, [cam_np, cam, True])
         game.accept("arrow_left-up", self._stop, [cam_np, cam, False])
         game.accept("arrow_right-up", self._stop, [cam_np, cam, False])
+
+        # key pressed - start turning
+        game.accept("alt-arrow_left", self._turn, [cam_np, -360, 0])
+        game.accept("alt-arrow_right", self._turn, [cam_np, 360, 0])
+        game.accept("alt-arrow_up", self._turn, [cam_np, 0, -60])
+        game.accept("alt-arrow_down", self._turn, [cam_np, 0, 25])
         return cam_np
 
     def _move(self, cam_np, cam, x, y, time):
@@ -63,8 +70,22 @@ class CameraController:
         self._move_int = LerpPosInterval(cam, time, self._target, other=cam_np)
         self._move_int.start()
 
+    def _turn(self, cam_np, h, r):
+        """Turn camera with a single interval (on key press).
+
+        Args:
+            cam_np (panda3d.core.NodePath): Camera node.
+            h (int): Translation for camera heading, angle.
+            r (int): Translation for camera rolling, angle.
+        """
+        if self._turn_int is not None and self._turn_int.isPlaying():
+            return
+
+        self._turn_int = LerpHprInterval(cam_np, 4, (cam_np.getH() + h, 0, r))
+        self._turn_int.start()
+
     def _stop(self, cam_np, cam, stop_x):
-        """Stop moving camera (on key release).
+        """Stop moving and rotating camera (on key release).
 
         Args:
             cam_np (panda3d.core.NodePath): Camera node.
@@ -73,7 +94,12 @@ class CameraController:
                 True - movement along x axis should be stopped.
                 False - movement along y axis should be stopped.
         """
-        self._move_int.pause()
+        if self._move_int is not None:
+            self._move_int.pause()
+
+        if self._turn_int is not None:
+            self._turn_int.pause()
+
         if stop_x:
             self._target.setX(cam.getX())
         else:
