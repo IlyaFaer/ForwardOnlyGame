f4982def4f9f082740a6e7a23bfd7f118caa118b
IlyaFaer <ilya.faer@mail.ru>
2020-10-28 17:58:53 +0300

    Add water spring environment model. Closes #72

--- world/locations.py
+++ world/locations.py
@@ -34,7 +34,7 @@ LOCATIONS = {
             {"models": ("stump1",), "chance": 7, "square": "wide"},
             {"models": ("tire1",), "chance": 2, "square": "wide"},
             {"models": ("cart1",), "chance": 4, "square": "wide"},
-            {"models": ("chapel1",), "chance": 1, "square": "wide"},
+            {"models": ("chapel1", "spring1"), "chance": 1, "square": "wide"},
         ),
         # enemy territory configurations
         "et_with_quantity": (
