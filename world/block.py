611b0dc1486c3df52e35c1c9d8551801d42f3900
IlyaFaer <ilya.faer@mail.ru>
2021-10-18 17:12:21 +0300

    Add snow spots landscape. Closes #167

--- world/block.py
+++ world/block.py
@@ -15,7 +15,7 @@ from .objects import BARRIER_THRESHOLD, ROCKET_THRESHOLD, Barrier, Rocket
 
 ANGLES = (0, 90, 180, 270)
 SURFACES = {
-    "direct": ("surface1", "surface2", "surface3", "surface4", "surface5"),
+    "direct": ("surface1", "surface2", "surface3", "surface4", "surface5", "surface6"),
     "l90_turn": ("l90_turn_surface1", "l90_turn_surface2"),
     "r90_turn": ("r90_turn_surface1", "r90_turn_surface2"),
     "ls": ("surface1", "surface2", "surface3"),
