945d6df58e82b10f8fb6cd20017a411b4e1c8f65
IlyaFaer <ilya.faer@mail.ru>
2022-04-11 16:05:51 +0300

    Update additional files.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="Forward Only",
+    version="0.88",
+    author="IlyaFaer",
+    author_email="gurovilya9@gmail.com",
+    description="Forward Only - Seepage, the game",
+    license="https://github.com/IlyaFaer/ForwardOnlyGame/blob/master/LICENSE.md",
+    url="https://github.com/IlyaFaer/ForwardOnlyGame",
+    options={
+        "build_apps": {
+            "platforms": [
+                "win_amd64",
+                "win32",
+                "macosx_10_6_x86_64",
+                "manylinux1_x86_64",
+            ],
+            "include_patterns": [
+                "**/*.bam",
+                "**/*.ico",
+                "**/*.jpg",
+                "**/*.jpeg",
+                "**/*.mp3",
+                "**/*.ogg",
+                "**/*.png",
+                "**/*.ptf",
+                "**/*.ttf",
+            ],
+            "include_modules": {"*": ["__builtin__", "shelve", "dbm"]},
+            "gui_apps": {"Forward Only": "main.py"},
+            "plugins": ["pandagl", "p3fmod_audio", "p3openal_audio"],
+        }
+    },
+)
